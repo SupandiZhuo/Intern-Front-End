1a312209f56bbb88a32954cbbf2719d7e39cd9f6
SupandiZhuo <161045662+SupandiZhuo@users.noreply.github.com>
2025-11-12 18:04:03 +0700

    Move product.js to resources/js, update app.js and layout

--- intern-app/resources/views/layouts/app.blade.php
+++ intern-app/resources/views/layouts/app.blade.php
@@ -34,6 +34,8 @@
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    @vite('resources/js/app.js')
+
     @stack('scripts')
 </body>
 
